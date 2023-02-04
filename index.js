9e5a3b34e08eb9d227c6fe4f643defcb188d7deb
Roshan Kumar Paswan <111443249+rajroshan0@users.noreply.github.com>
2023-02-04 09:43:47 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+var superheroes=require("superheroes");
+const supervillains = require("supervillains");
+
+var mysuperheroes= superheroes.random();
+var myvillains=supervillains.random();
+
+console.log(mysuperheroes);
+console.log(myvillains);
